90b1c33a49629812f00cf634db124f9952f9ecb3
FMA126 <mhblanks@gmail.com>
2019-06-07 13:35:18 -0400

    Create trailer model, controller, route, and seri
    1. scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 2019_06_07_154818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,21 @@ ActiveRecord::Schema.define(version: 2) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_examples_on_user_id"
+  end
+
+  create_table "trailers", force: :cascade do |t|
+    t.bigint "user_id"
+    t.text "make"
+    t.text "model"
+    t.integer "year"
+    t.text "trailer_type"
+    t.text "hitch_type"
+    t.integer "length"
+    t.integer "gvwr"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "axels"
+    t.index ["user_id"], name: "index_trailers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +49,5 @@ ActiveRecord::Schema.define(version: 2) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "trailers", "users"
 end
