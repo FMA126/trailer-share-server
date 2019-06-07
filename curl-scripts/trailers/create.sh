d5278ab4ddcc5ec6779003ed0b6f1cbacaddc846
FMA126 <mhblanks@gmail.com>
2019-06-07 18:12:51 -0400

    Fix merge conflict

--- curl-scripts/trailers/create.sh
+++ curl-scripts/trailers/create.sh
@@ -4,19 +4,11 @@ curl "http://localhost:4741/trailers/" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
-<<<<<<< HEAD
-  --data '{
-    "trailer": {
-      "make": "'"${MAKE}"'",
-      "model": "'"${MODEL}"'",
-      "user_id": "'"${USER}"'"
-=======
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "trailer": {
       "make": "'"${MAKE}"'",
       "model": "'"${MODEL}"'"
->>>>>>> dev
     }
   }'
 
