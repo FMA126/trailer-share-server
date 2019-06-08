a89f39eff109b6dc54130e7ebcf8155f64258015
FMA126 <mhblanks@gmail.com>
2019-06-08 11:28:12 -0400

    Fixed serializers

--- app/serializers/trailer_serializer.rb
+++ app/serializers/trailer_serializer.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class TrailerSerializer < ActiveModel::Serializer
-  attributes :id, :make, :model, :year, :trailer_type, :hitch_type, :length, :gvwr
+  attributes :id, :make, :model, :year, :trailer_type, :hitch_type, :length, :gvwr, :axels
+  belongs_to :user
   def editable
     scope == object.user
   end
